e8f839efd4cd75482ec12641851ebc0825203407
palagani lokeswara rao <43773069+palaganilokesh@users.noreply.github.com>
2023-05-12 21:29:00 +0530

    Update config-bkp.php

--- razorpay/config-bkp.php
+++ razorpay/config-bkp.php
@@ -1,11 +1,11 @@
 <?php
 
-$keyId = 'rzp_live_fSQh4q50RMjkVu';
-$keySecret = 'Nfrwn2AT1PcVrJ8nQUn3U4QW';
-$displayCurrency = 'INR';
+//$keyId = 'rzp_live_fSQh4q50RMjkVu';
+//$keySecret = 'Nfrwn2AT1PcVrJ8nQUn3U4QW';
+//$displayCurrency = 'INR';
 
 //These should be commented out in production
 // This is for error reporting
 // Add it to config.php to report any errors
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
